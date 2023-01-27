5977be304e62017e91c04ee14bdf6e8a6b1b30c2
Shreyansh <shreyansh.singhrajput2019@gmail.com>
2023-01-27 15:53:42 +0530

    update

--- src/Components/table.js
+++ src/Components/table.js
@@ -136,18 +136,24 @@ function MyTable() {
                 <Table>
                   <TableBody>
                     <TableRow>
-                      <TableCell variant="head">Label</TableCell>
-                      <TableCell align="right">Value</TableCell>
+                      <TableCell>Label</TableCell>
+                      <TableCell variant="head" align="right">
+                        Value
+                      </TableCell>
                     </TableRow>
 
                     <TableRow>
-                      <TableCell variant="head">Label</TableCell>
-                      <TableCell align="right">Value</TableCell>
+                      <TableCell>Label</TableCell>
+                      <TableCell variant="head" align="right">
+                        Value
+                      </TableCell>
                     </TableRow>
 
                     <TableRow>
-                      <TableCell variant="head">Label</TableCell>
-                      <TableCell align="right">Value</TableCell>
+                      <TableCell>Label</TableCell>
+                      <TableCell variant="head" align="right">
+                        Value
+                      </TableCell>
                     </TableRow>
                   </TableBody>
                 </Table>
@@ -165,18 +171,24 @@ function MyTable() {
                 <Table>
                   <TableBody>
                     <TableRow>
-                      <TableCell variant="head">Label</TableCell>
-                      <TableCell align="right">Value</TableCell>
+                      <TableCell>Label</TableCell>
+                      <TableCell variant="head" align="right">
+                        Value
+                      </TableCell>
                     </TableRow>
 
                     <TableRow>
-                      <TableCell variant="head">Label</TableCell>
-                      <TableCell align="right">Value</TableCell>
+                      <TableCell>Label</TableCell>
+                      <TableCell variant="head" align="right">
+                        Value
+                      </TableCell>
                     </TableRow>
 
                     <TableRow>
-                      <TableCell variant="head">Label</TableCell>
-                      <TableCell align="right">Value</TableCell>
+                      <TableCell>Label</TableCell>
+                      <TableCell variant="head" align="right">
+                        Value
+                      </TableCell>
                     </TableRow>
                   </TableBody>
                 </Table>
